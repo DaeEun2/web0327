c5f7885b4ebe68b2dd26039953b34aabb4a405b2
DaeEun2 <37567415+DaeEun2@users.noreply.github.com>
2018-03-27 14:39:51 +0900

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,11 +1,26 @@
 var http = require('http');
-
+/*
 function requestCallbackFunction (req, res) {
   // if(req.url === '/favicon.ico') {
   //   console.log('Favicon was requested');
   // } else {
     console.log('hello');
   // }
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}
+
+var server = http.createServer(
+  requestCallbackFunction
+);
+*/
+
+function requestCallbackFunction (req, res) {
+   if(req.url === '/favicon.ico') {
+     console.log('Favicon was requested');
+   } else {
+    console.log('hello');
+   }
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!');
 }
